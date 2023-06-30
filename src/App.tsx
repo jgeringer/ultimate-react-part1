15246dc28e0f9a3368e389b8bf9baf0c5e42acfd
Joseph Geringer <joegeringer@gmail.com>
2023-06-30 15:55:12 -0500

    first part of backend, useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,44 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import { useEffect, useRef, useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 15, category: "Grocery" },
-    { id: 3, description: "ccc", amount: 20, category: "Entertainment" },
-    { id: 4, description: "ddd", amount: 68, category: "Utilities" },
-  ]);
+  // this is a side effect. it's changing something outside of this component (DOM in this case).
+  // it's not a pure component anymore, so let's use useEffect
+  // if (ref.current) ref.current.focus();
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  const [category, setCategory] = useState("");
+
+  // might also be called after render
+  // useEffect(() => {
+  //   // if (ref.current) ref.current.focus();
+  // });
+
+  const connect = () => console.log("connecting...");
+  const disconnect = () => console.log("disconecting...");
+
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  });
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
+      <input ref={ref} type="text" className="form-control" />
+
+      <select
+        name=""
+        id=""
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="clothing">clothing</option>
+        <option value="household">household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }
