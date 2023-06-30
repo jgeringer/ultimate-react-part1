e3b3eacbff0394635d2a47784184be73d52ebda1
Joseph Geringer <joegeringer@gmail.com>
2023-06-30 16:06:53 -0500

    fetch sample data with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,27 @@
-import { useEffect, useRef, useState } from "react";
-import ProductList from "./components/ProductList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+// use an interface to define the shape of our object
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
-
-  // this is a side effect. it's changing something outside of this component (DOM in this case).
-  // it's not a pure component anymore, so let's use useEffect
-  // if (ref.current) ref.current.focus();
-
-  const [category, setCategory] = useState("");
-
-  // might also be called after render
-  // useEffect(() => {
-  //   // if (ref.current) ref.current.focus();
-  // });
-
-  const connect = () => console.log("connecting...");
-  const disconnect = () => console.log("disconecting...");
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    connect();
-
-    return () => disconnect();
-  });
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data));
+  }, []);
 
   return (
-    <div>
-      <input ref={ref} type="text" className="form-control" />
-
-      <select
-        name=""
-        id=""
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="clothing">clothing</option>
-        <option value="household">household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
