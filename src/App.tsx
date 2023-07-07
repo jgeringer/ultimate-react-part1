2909c044a31de745d403c6239c9e160f1f8362b9
Joseph Geringer <joegeringer@gmail.com>
2023-07-07 13:23:49 -0500

    add update delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
+import apiClient, { CanceledError } from "./services/api-client";
 
 // use an interface to define the shape of our object
 interface User {
@@ -10,34 +10,95 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // axios
-    //   .get<User[]>("https://jsonplaceholder.typiXcode.com/users")
-    //   .then((response) => setUsers(response.data))
-    //   .catch((err) => setError(err.message));
+    const controller = new AbortController(); // built in class in browsers that allows us to cancel/abort fetch requests.
+    setIsLoading(true);
 
-    // instead of above, async await...
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-    };
+    apiClient
+      .get<User[]>("/users", {
+        signal: controller.signal,
+      })
+      .then((response) => {
+        setUsers(response.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
 
-    fetchUsers();
+    return () => controller.abort();
   }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    apiClient.delete(`/users/${user.id}`).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "Mosh" };
+    setUsers([newUser, ...users]);
+
+    apiClient
+      .post("/users", newUser)
+      // .then((res) => setUsers([res.data, ...users]));
+      // or write the above this way...
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users])) // savedUser is just an alias
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+
+    apiClient.patch("/users/" + user.id, updatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
 
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
-      <ul>
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary m" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <span>{user.name}</span>
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-2"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
         ))}
       </ul>
     </>
