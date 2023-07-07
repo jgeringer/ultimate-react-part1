caa6ccec2d9ffbcde993b4beeda891720f178fce
Joseph Geringer <joegeringer@gmail.com>
2023-07-07 14:19:40 -0500

    added custom hook

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import userService, { User } from "../services/user-service";
+import { CanceledError } from "../services/api-client";
+
+
+const useUsers = () => {
+    const [users, setUsers] = useState<User[]>([]);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+  
+    useEffect(() => {
+      setIsLoading(true);
+  
+      const { request, cancel } = userService.getAll<User>();
+      request
+        .then((response) => {
+          setUsers(response.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+          setIsLoading(false);
+        });
+  
+      return () => cancel();
+    }, []);
+
+    // This is needed to reuse our state variables...
+    return {
+        users, error, isLoading, setUsers, setError
+    }
+}
+
+export default useUsers;
